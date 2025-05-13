516846bc958a1d9395778ee0d21c9e0c50f20933
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-05-13 08:19:47 -0400

    js

---
+++ script.js
@@ -0,0 +1,25 @@
+function generatepassword(length, lower, upper, number, symbol) {
+    // Create a variable to store the password
+    let password = "";
+    // Create a variable to store the types of characters that will be used in the password
+    const typesCount = lower + upper + number + symbol;
+    // Create a variable to store the types of characters that will be used in the password
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+        item => Object.values(item)[0]
+    );
+    // If no character types are selected, return an empty string
+    if (typesCount === 0) {
+        return "";
+    }
+    // Loop through the length of the password
+    for (let i = 0; i < length; i += typesCount) {
+        // Loop through each character type and add a random character to the password
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            password += randomFunctions[funcName]();
+        });
+    }
+    // Return the password with the correct length
+    return password.slice(0, length);
+    
+}
