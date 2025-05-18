34aea22de168ec423135b38a1359976156a9022f
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-05-17 23:15:30 -0400

    js

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-function generatepassword(passwordLength, lower, upper, number, special) {
+// Helper functions to get random characters
+function getRandomLower() {
+    const letters = `abcdefghijklmnopqrstuvwxyz`;
+    return letters[Math.floor(Math.random() * letters.length)];
+}
+
+function getRandomUpper() {
+    const letters = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    return letters[Math.floor(Math.random() * letters.length)];
+}
+
+function getRandomNumber() {
+    const numbers = `0123456789`;
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getRandomSpecial() {
+    const specials = `!@#$%^&*()_+[]{}|;:,.<>?`;
+    return specials[Math.floor(Math.random() * specials.length)];
+}
+
+// Object to map checkbox to function 
+const randomFunctions = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    special: getRandomSpecial
+};
+
+//generate password function 
+function generatePassword(length, lower, upper, number, special) {
     // Create a variable to store the password
     let password = "";
     // Create a variable to store the types of characters that will be used in the password
@@ -11,6 +41,7 @@ function generatepassword(passwordLength, lower, upper, number, special) {
     if (typesCount === 0) {
         return "";
     }
+    //Build the password
     // Loop through the length of the password
     for (let i = 0; i < length; i += typesCount) {
         // Loop through each character type and add a random character to the password
@@ -20,12 +51,48 @@ function generatepassword(passwordLength, lower, upper, number, special) {
         });
     }
     // Return the password with the correct length
-    return password.slice(0, passwordLength);
-    
+    return password.slice(0, length);
+
 }
-function getRandomLower() {
-    const letters = `abcdefghijklmnopqrstuvwxyz`;
-    return letters[randomIndex(letters)];
+
+// DOM Elements
+const resultEl = document.getElementById(`result`);
+const clipboardEl = document.getElementById(`clipboard`);
+const lengthEl = document.getElementById(`length`);
+const upperEl = document.getElementById(`upper`);
+const lowerEl = document.getElementById(`lower`);
+const numberEl = document.getElementById(`number`);
+const specialEl = document.getElementById(`special`);
+const generateEl = document.getElementById(`generate`);
+const passwordLengthEl = document.getElementById(`password-length`);
     
-}
-console.log(getRandomLower()); //Random lowercase letter
+
+// Event listener for the generate button
+generateEl.addEventListener(`click`, () => {
+    // Get the values of the checkboxes and length input
+    const length = +lengthEl.value;
+    const lower = lowerEl.checked;
+    const upper = upperEl.checked;
+    const number = numberEl.checked;
+    const special = specialEl.checked;
+
+    // Generate the password
+    const password = generatePassword(length, lower, upper, number, special);
+    // Display the password in the result element
+    resultEl.innerText = password;
+});
+// Function to copy the password to the clipboard
+clipboardEl.addEventListener(`click`, () => {
+    // Accessing the text/string value (AKA the Password) for the "result" span and setting it the the "password" variable
+    const password = resultEl.innerText;
+
+    // If the user clicks the clipboard button while no password is displayed, then an alert will be displayed to the user and function will end and nothing will be copied to the clipboard
+    if (password === ``) {
+        alert(`Please generate a password first`);
+        return;
+    }
+
+    // Referencing the "navigator" object to copy the selected value to the clipboard on the device the webpage is being viewed on
+    navigator.clipboard.writeText(password);
+});
+
